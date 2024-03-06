58ada07a2a5d2af74735443dc16e557453cd70ec
preethi <preethibrgwd585@gmail.com>
2024-03-06 13:01:42 +0530

    Add project files.

---
+++ BusinessLogicLayer/EmployeeDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DataAccessLayer;
+
+
+namespace BusinessLogicLayer
+{
+  public  class EmployeeDTO
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmpCode { get; set; }
+        public System.DateTime DateOfBirth { get; set; }
+        public string EmpName { get; set; }
+        public string Email { get; set; }
+        public Nullable<int> DeptCode { get; set; }
+
+        public virtual DeptMaster DeptMaster { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
---
+++ EmployeeManagementSystem/Models/DeptMasterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class DeptMasterModel
+    {
+        public int DeptCode { get; set; }
+        public string DeptName { get; set; }
+    }
+}
